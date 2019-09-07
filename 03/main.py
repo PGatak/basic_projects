6e290f13a6cc19939dfa5a18998e7a27513f6adb
PGatak <pozycjonowanieadsum@gmail.com>
2019-09-07 10:03:50 +0200

    Shuffle data

--- 03/main.py
+++ 03/main.py
@@ -6,10 +6,20 @@ import sklearn.linear_model as slm
 
 data = pd.read_csv('data/heart.csv')
 data = data.dropna()
-print(data)
+#print(data)
 
-rows = random.sample(2)
-print(rows)
+data_mtx = np.matrix(data)
+data_lst = data_mtx.tolist()
+#print(data_lst)
+random.shuffle(data_lst)
+#print(data_lst)
+training_data = data_lst[:100]
+print(training_data)
+test_data = data_lst[100:]
+
+training_data_answer = training_data
+print(training_data_answer)
+
 
 
 
